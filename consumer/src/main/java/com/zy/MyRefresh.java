e151dd9d4e7b06510d60f54bc2e4028334ab4e4b
525711580Zy <48376580+525711580Zy@users.noreply.github.com>
2019-03-09 18:23:36 +0800

    Update MyRefresh.java

--- consumer/src/main/java/com/zy/MyRefresh.java
+++ consumer/src/main/java/com/zy/MyRefresh.java
@@ -3,5 +3,6 @@ package com.zy;
 public class MyRefresh {
     public static void main(String[] args) {
         System.out.println("试验git提交是否陈宫");
+        System.out.println("试验git提交是否陈宫");
     }
 }
