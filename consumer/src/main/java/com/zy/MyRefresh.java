2fb5fe131034a35af95b28e91df5e334e8ab8474
张宇 <48376580+525711580Zy@users.noreply.github.com>
2019-03-09 18:33:34 +0800

    Update MyRefresh.java

--- consumer/src/main/java/com/zy/MyRefresh.java
+++ consumer/src/main/java/com/zy/MyRefresh.java
@@ -4,5 +4,6 @@ public class MyRefresh {
     public static void main(String[] args) {
         System.out.println("试验git提交是否陈宫");
         System.out.println("试验git提交是否陈宫");
+        System.out.println("试验git提交是否陈宫");
     }
 }
