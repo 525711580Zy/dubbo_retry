a343857693f422f81897a6151fb747cda6f57841
zhangyu <zhangyu90@126.com>
2019-03-12 15:07:53 +0800

    Idea整合github测试

--- consumer/src/main/java/com/zy/MyRefresh.java
+++ consumer/src/main/java/com/zy/MyRefresh.java
@@ -1,10 +1,13 @@
 package com.zy;
 
+import java.util.HashMap;
+
 public class MyRefresh {
     public static void main(String[] args) {
         System.out.println("试验git提交是否陈宫");
         System.out.println("试验git提交是否陈宫");
         System.out.println("试验git提交是否陈宫");
         System.out.println("试验只commit不push");
+
     }
 }
